83d1fdb87cb7f91cfd354ca18b63813b40c59c14
Juliana Tamizaki <jutamizaki@gmail.com>
2018-01-30 22:24:50 -0800

    landing page updated

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import logo from './img/grouplogo.png';
 import './App.css';
+import logo from './img/grouplogo.png';
+import jImg from './img/ju.svg';
+import tImg from './img/tiff.svg';
 import ChatPage from './comp/ChatPage';
 
 class App extends Component {
@@ -26,20 +28,24 @@ class App extends Component {
           comp = (
               <div>
               <br/><br/>
+            <img className="logo" src={logo} alt="logo" />
          <h1 className="title">Juliana and Tiffany's App</h1>
         <Container>
         <Row>
-          <Col>
-            <br /><br />
-             <img className="logo" src={logo} alt="logo" />
+          <Col className="col-3">
+             <img src={jImg} alt="jImg" clasName="lanImg"/>
           </Col>
         
-          <Col>
+          <Col className="col-6">
             <h2 className="blue">We design and develop applications</h2>
               <p className="parag">
                 Currently Juliana and Tiffany are finishing up their diploma at BCIT for Digital Design and Development. They learned many necessary and great skills surrounding development, and that involves HTML, CSS, JavaScript, React, and many more. Also specializing in adobe softwares like Photoshop, Illustrator, InDesign, After Effects and Premiere Pro. Even learning 
                 many web development tools, Tiffany's heart still lies in Digital Marketing, and Juliana is more passionate about Graphic Design.
               </p>
+          </Col>
+          
+          <Col className="col-3">
+                <img src={tImg} alt="tImg"clasName="lanImg" />
           </Col>
               
         </Row>
