cbb2bf5867ef4b6bc6e0265847863934253cd224
Juliana Tamizaki <jutamizaki@gmail.com>
2018-02-03 14:47:47 -0800

    landingpage updated 2

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -28,24 +28,24 @@ class App extends Component {
           comp = (
               <div>
               <br/><br/>
-            <img className="logo" src={logo} alt="logo" />
-         <h1 className="title">Juliana and Tiffany's App</h1>
+                <img className="logo" src={logo} alt="logo" />
+                <h1 className="title">Juliana and Tiffany's App</h1>
+                <h2 className="blue">We design and develop applications</h2>
         <Container>
         <Row>
-          <Col className="col-3">
+          <Col>
              <img src={jImg} alt="jImg" clasName="lanImg"/>
           </Col>
         
-          <Col className="col-6">
-            <h2 className="blue">We design and develop applications</h2>
+          <Col>
               <p className="parag">
                 Currently Juliana and Tiffany are finishing up their diploma at BCIT for Digital Design and Development. They learned many necessary and great skills surrounding development, and that involves HTML, CSS, JavaScript, React, and many more. Also specializing in adobe softwares like Photoshop, Illustrator, InDesign, After Effects and Premiere Pro. Even learning 
                 many web development tools, Tiffany's heart still lies in Digital Marketing, and Juliana is more passionate about Graphic Design.
               </p>
           </Col>
           
-          <Col className="col-3">
-                <img src={tImg} alt="tImg"clasName="lanImg" />
+          <Col>
+                <img src={tImg} alt="tImg" clasName="lanImg" />
           </Col>
               
         </Row>
